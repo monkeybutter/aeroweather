e673f19f4e347e4db13591e5a580c4d43a642add
monkeybutter <p.rozas.larraondo@gmail.com>
2014-11-14 16:32:43 +1100

    metar_scrapper modified

--- metar_scrapper.py
+++ metar_scrapper.py
@@ -4,21 +4,21 @@ import urllib2
 import re
 import datetime
 
-metar = open("metar_temp.txt", "w")
+with open("petar_temp.txt", "w") as metar:
         
-startDate = datetime.date(2011, 1, 1)
-endDate = datetime.date(2011, 9, 1)
-delta = datetime.timedelta(days=1)
+    startDate = datetime.date(2011, 6, 16)
+    endDate = datetime.date(2012, 2, 1)
+    delta = datetime.timedelta(days=1)
 
-delta = datetime.timedelta(days=1)
-while startDate <= endDate:
-    print startDate.strftime("%y-%m-%d ")
-    request = startDate.strftime("http://vortex.plymouth.edu/cgi-bin/gen_statlog-u.cgi?ident=LEBL&pl=none0&yy=" + "%y" + "&mm=" + "%m" + "&dd=" + "%d")
-    response = urllib2.urlopen(request)
-    for line in response.read().split('\n'):
-        match = re.search('^LEBL', line)
-        if match:
-            metar.write('\n' + startDate.strftime("%y-%m-%d ") + line)
-        else:
-            metar.write(line)
-    startDate += delta
+    delta = datetime.timedelta(days=1)
+    while startDate <= endDate:
+        print startDate.strftime("%y-%m-%d ")
+        request = startDate.strftime("http://vortex.plymouth.edu/cgi-bin/gen_statlog-u.cgi?ident=LEBL&pl=none0&yy=" + "%y" + "&mm=" + "%m" + "&dd=" + "%d")
+        response = urllib2.urlopen(request)
+        for line in response.read().split('\n'):
+            match = re.search('^LEBL', line)
+            if match:
+                metar.write('\n' + startDate.strftime("%y-%m-%d ") + line)
+            else:
+                print(line)
+        startDate += delta
