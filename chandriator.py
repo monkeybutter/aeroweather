6655f0d462cb5ac61c93c48173e5f482b23ee1d3
monkeybutter <p.rozas.larraondo@gmail.com>
2014-11-14 17:09:29 +1100

    2

--- chandriator.py
+++ chandriator.py
@@ -1,12 +1,5 @@
-
-
-with open("metar_temp2.txt", "wt") as fout:
-    with open("metar_temp.txt", "rt") as fin:
+with open("petar_temp3.txt", "wt") as fout:
+    with open("petar_temp.txt", "rt") as fin:
         for line in fin:
-            fout.write(line.replace('METAR', '\nMETAR').replace('<TITLE>', '\n<TITLE>').replace('SPECI', '\nSPECI'))
-
-with open("metar_temp3.txt", "wt") as fout:
-    with open("metar_temp2.txt", "rt") as fin:
-        for line in fin:
-            if line[:5] != 'METAR' and line[:7] != '<TITLE>' and line[:5] != 'SPECI' and "NIL" not in line:
+            if "NIL" not in line:
                 fout.write(' '.join(line.split()) + "\n")
