ab8e1450e713cbb3884a155227300005bafd1b13
monkeybutter <p.rozas.larraondo@gmail.com>
2014-11-19 09:54:10 +1100

    milan chuck 1

--- metar_scrapper.py
+++ metar_scrapper.py
@@ -6,13 +6,13 @@ import datetime
 
 with open("metar_temp.txt", "w") as metar:
         
-    startDate = datetime.date(2012, 11, 29)
-    endDate = datetime.date(2013, 7, 1)
+    startDate = datetime.date(2011, 1, 1)
+    endDate = datetime.date(2011, 7, 1)
     delta = datetime.timedelta(days=1)
 
     while startDate <= endDate:
         print startDate.strftime("%y-%m-%d ")
-        request = startDate.strftime("http://vortex.plymouth.edu/cgi-bin/gen_statlog-u.cgi?ident=LFPG&pl=none0&yy=" + "%y" + "&mm=" + "%m" + "&dd=" + "%d")
+        request = startDate.strftime("http://vortex.plymouth.edu/cgi-bin/gen_statlog-u.cgi?ident=LIMC&pl=none0&yy=" + "%y" + "&mm=" + "%m" + "&dd=" + "%d")
         response = urllib2.urlopen(request)
 
         semaphore = False
@@ -23,7 +23,7 @@ with open("metar_temp.txt", "w") as metar:
                 metar.write(' ' + ' '.join(line.split()))
 
             else:
-                match = re.search('^METAR LFPG', line)
+                match = re.search('^METAR LIMC', line)
                 if match:
                     semaphore = True
                     metar.write('\n' + startDate.strftime("%y-%m-%d ") + ' '.join(line.split()))
